7daebaf511cc4c3896ffeacba9e94d99c47eea05
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2023-05-29 19:35:11 +0300

    Add config files

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/godazz/goGin/config"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	configs := setConfig()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message":  "pong",
+			"app name": viper.Get("App.Name"),
 		})
 	})
-	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
+}
+
+func setConfig() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config") // path to look for the config file in
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading the config")
+	}
+
+	var configs config.Config
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Printf("unable to decote configuration %v\n", err)
+	}
+	return configs
 }
