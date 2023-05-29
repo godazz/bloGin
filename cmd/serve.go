c06f10dbe91b2932544de9d8c8516ec1a50d9327
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2023-05-29 19:47:41 +0300

    Add cobra commands to start app

---
+++ cmd/serve.go
@@ -0,0 +1,58 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/godazz/goGin/config"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Serve app on dev server",
+	Long:  "Application will be served on host and port defined in config.yaml file",
+	Run: func(cmd *cobra.Command, args []string) {
+		serve()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
+
+func serve() {
+	configs := setConfig()
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "pong",
+			"app name": viper.Get("App.Name"),
+		})
+	})
+	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
+}
+
+func setConfig() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config") // path to look for the config file in
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading the config")
+	}
+
+	var configs config.Config
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Printf("unable to decote configuration %v\n", err)
+	}
+	return configs
+}
