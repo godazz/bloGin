a3018fa0aa27369f20ae57b90036314feaa2ae05
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2023-05-31 10:45:07 +0300

    Serve html templates and static files

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -2,13 +2,19 @@ package bootstrap
 
 import (
 	"github.com/godazz/bloGin/pkg/config"
+	"github.com/godazz/bloGin/pkg/html"
 	"github.com/godazz/bloGin/pkg/routing"
+	"github.com/godazz/bloGin/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 
 	routing.Init()
+
+	static.LoadStatic(routing.GetRouter())
+
+	html.LoadHTML(routing.GetRouter())
 
 	routing.RegisterRoutes()
 
